fc0c90ca062d1ab6aefb941375890a6b993f5f77
Ibrahim Doghri <patron.bara@gmail.com>
2023-07-13 10:21:56 +0100

    animate plugin

--- src/lib/slide.svelte
+++ src/lib/slide.svelte
@@ -8,10 +8,12 @@
 
 	import Simplemenu from './revealjs-plugins/simplemenu/plugin/simplemenu/simplemenu.esm.js';
 	import Verticator from './revealjs-plugins/verticator/plugin/verticator/verticator.esm.js';
+	import Animate from './revealjs-plugins/animate/animate.esm.js';
 /* ------------------------------- stylesheets ------------------------------ */
     import 'reveal.js/dist/reveal.css';
     import 'reveal.js/dist/theme/black.css';
     import 'reveal.js/plugin/highlight/monokai.css';
+	import 'animate.css';
 
 	import './revealjs-plugins/verticator/plugin/verticator/verticator.css';
 	import './revealjs-plugins/simplemenu/plugin/simplemenu/simplemenu.css';
@@ -29,37 +31,37 @@
 		});
 
 		deck.initialize({
-			plugins: [ Verticator, Simplemenu],//Markdown, Highlight, Notes, Animate,
+			plugins: [ Animate, Verticator, Simplemenu],//Markdown, Highlight, Notes,
 			verticator: {
 				csspath: './revealjs-plugins/verticator/plugin/verticator/verticator.css'
 			},
 			simplemenu: {
 				csspath: false
-			}
+			},
 			
-			// animated: {
-			// 	H1: { animated: 'slideInDown' },
-			// 	IMG: { animated: 'backInDown', repeat: 'repeat-2', speed: 'slower' },
-			// 	P: { animated: 'bounceInRight', speed: 'fast' },
-			// 	UL: { animated: 'jello', repeat: 'infinite', speed: 'slower' }
-			// },
+			animated: {
+				H1: { animated: 'slideInDown' },
+				IMG: { animated: 'backInDown', repeat: 'repeat-2', speed: 'slower' },
+				P: { animated: 'bounceInRight', speed: 'fast' },
+				UL: { animated: 'jello', repeat: 'infinite', speed: 'slower' }
+			},
 		});
 	});
 </script>
 
 <div class="reveal">
 	<div class="slides">
-		<section id="0">1</section>
-		<section id="1">2</section>
-		<section id="2">3</section>
+		<section id="0"><h1>1</h1></section>
+		<section id="1"><ul>2</ul></section>
+		<section id="2"><h1>3</h1></section>
 		<section id="3">
-            <section>4.1</section>
-            <section>4.2</section>
-            <section>4.3</section>
-            <section>4.4</section>
+            <section><p>4.1</p></section>
+            <section><p>4.2</p></section>
+            <section><p>4.3</p></section>
+            <section><p>4.4</p></section>
 
         </section>
-        <section id="4">5</section>
+        <section id="4"><ul>5</ul></section>
 
 	</div>
 </div>
